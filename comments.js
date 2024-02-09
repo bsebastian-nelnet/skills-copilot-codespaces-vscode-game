7a470a3bea04fd9449fc63cc09c3f2982c98df65
Bentic Sebastian <110062441+bsebastian-nelnet@users.noreply.github.com>
2024-02-09 22:11:32 +0000

    test

---
+++ comments.js
@@ -0,0 +1,191 @@
+//create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = require('./comments');
+var mime = require('mime');
+var cache = {};
+var server = http.createServer(function(request, response) {
+    var filePath = false;
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+server.listen(3000, function() {
+    console.log("Server listening on port 3000.");
+});
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+function send404(response) {
+    response.writeHead(404, {
+        'Content-Type': 'text/plain'
+    });
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200, {
+        "content-type": mime.lookup(path.basename(filePath))
+    });
+    response.end(fileContents);
+}
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        });
+    }
+}
+// Path: comments.js
+var comments = [];
+exports.addComment = function(comment) {
+    comments.push(comment);
+};
+exports.getComments = function() {
+    return comments;
+};
+// Path: chat_server.js
+var socketio = require('socket.io');
+var io;
+var guestNumber = 1;
+var nickNames = {};
+var namesUsed = [];
+var currentRoom = {};
+exports.listen = function(server) {
+    io = socketio.listen(server);
+    io.set('log level', 1);
+    io.sockets.on('connection', function(socket) {
+        guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
+        joinRoom(socket, 'Lobby');
+        handleMessageBroadcasting(socket, nickNames);
+        handleNameChangeAttempts(socket, nickNames, namesUsed);
+        handleRoomJoining(socket);
+        socket.on('rooms', function() {
+            socket.emit('rooms', io.sockets.manager.rooms);
+        });
+        handleClientDisconnection(socket, nickNames, namesUsed);
+    });
+};
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+    var name = 'Guest' + guestNumber;
+    nickNames[socket.id] = name;
+    socket.emit('nameResult', {
+        success: true,
+        name: name
+    });
+    namesUsed.push(name);
+    return guestNumber + 1;
+}
+function joinRoom(socket, room) {
+    socket.join(room);
+    currentRoom[socket.id] = room;
+    socket.emit('joinResult', {
+        room: room
+    });
+    socket.broadcast.to(room).emit('message', {
+        text: nickNames[socket.id] + ' has joined ' + room + '.'
+    });
+    var usersInRoom = io.sockets.clients(room);
+    if (usersInRoom.length > 1) {
+        var usersInRoomSummary = 'Users currently in ' + room + ': ';
+        for (var index in usersInRoom) {
+            var userSocketId = usersInRoom[index].id;
+            if (userSocketId != socket.id) {
+                if (index > 0) {
+                    usersInRoomSummary += ', ';
+                }
+                usersInRoomSummary += nickNames[userSocketId];
+            }
+        }
+        usersInRoomSummary += '.';
+        socket.emit('message', {
+            text: usersInRoomSummary
+        });
+    }
+}
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+    socket.on('nameAttempt', function(name) {
+        if (name.indexOf('Guest') == 0) {
+            socket.emit('nameResult', {
+                success: false,
+                message: 'Names cannot begin with "Guest".'
+            });
+        } else {
+            if (namesUsed.indexOf(name) == -1) {
+                var previousName = nickNames[socket.id];
+                var previousNameIndex = namesUsed.indexOf(previousName);
+                namesUsed.push(name);
+                nickNames[socket.id] = name;
+                delete namesUsed[previousNameIndex];
+                socket.emit('nameResult', {
+                    success: true,
+                    name: name
+                });
+                socket.broadcast.to(currentRoom[socket.id]).emit('message', {
+                    text: previousName + ' is now known as ' + name + '.'
+                });
+            } else {
+                socket.emit('nameResult', {
+                    success: false,
+                    message: 'That name is already in use.'
+                });
+            }
+        }
+    });
+}
+function handleMessageBroadcasting(socket) {
+    socket.on('message', function(message) {
+        socket.broadcast.to(message.room).emit('message', {
+            text: nickNames[socket.id] + ': ' + message.text
+        });
+    });
+}
+function handleRoomJoining(socket) {
+    socket.on('join', function(room) {
+        socket.leave(currentRoom[socket.id]);
+        joinRoom(socket, room.newRoom);
+    });
+}
+function handleClientDisconnection(socket) {
+    socket.on('disconnect', function() {
+        var nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+        delete namesUsed[nameIndex];
+        delete nickNames[socket.id];
+    });
+}
+// Path: public/index.html
+// Remove the HTML declaration line
+// <DOCTYPE html>
+
+<html>      
+<head>
+    <title>Chat</title>
+    <link rel="stylesheet" href="/stylesheets/style.css"></link>
+    </head>
+    <body>
+        <div id="room-list"></div>
+        <div id="message-list"></div>
+        <div id="user-list"></div>
+        <script src="/socket.io/socket.io.js"></script>
+        <script src="/javascripts/chat.js"></script>
+        </body>
+
+    </html>
